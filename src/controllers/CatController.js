906a39d78d4d0f12c38a630eae72aaf6c6baa6a1
Alex H <alex.holder@bigfootds.com>
2023-11-02 20:59:40 +1100

    feat: cat controller routes scaffolded

--- src/controllers/CatController.js
+++ src/controllers/CatController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Cat } = require('../models/CatModel');
 const router = express.Router();
 
+// Find ALL cats in the DB
 router.get("/all", async (request, response) => {
 	// Empty object in .find() means get ALL documents
 	let result = await Cat.find({});
@@ -11,5 +12,72 @@ router.get("/all", async (request, response) => {
 	});
 
 });
+
+// Find one cat by its ID
+router.get("/one/id/:id", async (request, response) => {
+	let result = null;
+
+	response.json({
+		cat: result
+	});
+
+});
+
+// Find one cat by its name 
+// localhost:3000/cats/one/name/Lina
+router.get("/multiple/name/:nameToSearchFor", async (request, response) => {
+	let result = null;
+
+	response.json({
+		cat: result
+	});
+
+});
+
+router.get("/multiple/breed/:breedToFilterBy", async (request, response) => {
+	let result = null;
+
+	response.json({
+		cat: result
+	});
+
+});
+
+
+// Create a new cat in the DB
+// POST localhost:3000/cats/
+router.post("/", async (request, response) => {
+	let result = await Cat.create(request.body);
+
+	response.json({
+		cat: result
+	});
+
+});
+
+// Update an existing cat in the DB.
+// Find one cat by its ID, and modify that cat. 
+// Patch is for whatever properties are provided,
+// does not overwrite or remove any unmentioned properties of the cat 
+router.patch("/:id", async (request, response) => {
+	let result = null;
+
+	response.json({
+		cat: result
+	});
+
+});
+
+// Find one cat by its ID,
+// and delete it from the DB.
+router.delete("/:id", async (request, response) => {
+	let result = null;
+
+	response.json({
+		cat: result
+	});
+
+});
+
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const express = require('express');
 // make a server instance 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
 	response.json({
 		message:"Hello world"
